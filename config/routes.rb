daa03831f2846ddd0e5827bd5d72d0e4a3e3ca6d
dev1980 <brham1980@gmail.com>
2019-12-05 03:04:58 +0545

    Comment form added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts, only: [:index, :show, :new, :create]
   resources :users, only: [:index, :show]
+  resources :comments, only: [:new, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
