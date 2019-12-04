ca6b081e32dfa765efc287c0420440630262bb63
Miguel Prada <mapra99@gmail.com>
2019-12-04 17:01:17 -0500

    implemented comments show in the posts show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :sign_up_if_not_logged_in
+  def new
+  end
+
+  def create
+    post = Post.find(params[:comment][:post_id])
+    if post
+      post.comments.create(content: params[:comment][:content],
+                           user_id: current_user.id)
+    end
+    redirect_back(fallback_location: root_path)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,6 @@ class Comment < ApplicationRecord
 
     validates :content, presence: true,
                         length: { maximum: 200 }
+
+    default_scope -> {order(created_at: :desc)}
 end
