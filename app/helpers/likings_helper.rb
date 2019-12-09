e7ee5c645a6e595c9968a894542ca61b778ffe5a
dev1980 <brham1980@gmail.com>
2019-12-10 03:35:29 +0545

    logic moved to helper file

--- app/helpers/likings_helper.rb
+++ app/helpers/likings_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module LikingsHelper
+    def like_dislike(post)
+        if current_user.already_like? post
+          link_to 'Dislike', liking_path(id: post.id), method: :delete
+        else
+          link_to 'Like', likings_path(post_id: post.id), method: :post
+        end
+      end
 end
