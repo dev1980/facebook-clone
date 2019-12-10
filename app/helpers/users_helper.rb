8bf0293adfdb945849da0b4dca270e91bfffcf74
dev1980 <brham1980@gmail.com>
2019-12-11 01:55:33 +0545

    style for user index implemented

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user, **options)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    image_tag(gravatar_url, alt: user.name, class: options[:class])
   end
 end
