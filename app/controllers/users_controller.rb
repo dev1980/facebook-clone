c996927e2fa2b660737f757e646b2afba2ba6946
Miguel Prada <mapra99@gmail.com>
2019-12-17 17:33:03 -0500

    refactored some code and fixed rspec test errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_id(params[:id])
     @posts = @user.posts.paginate(page: params[:page], per_page: 10)
   end
 end
